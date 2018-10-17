2d4ac9e8047fed178d833564fbf2944c6c9b6b12
Tan Nguyen <tan.trong.nguyen@mowede.com>
2018-10-17 16:35:44 +0700

    untrack files contained in the .gitignore file

--- cache/compiled/files/2b899e14ff46e65dd2263685027a67e5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://x-corporation/x-corporation.yaml',
-    'modified' => 1537979424,
-    'data' => [
-        'enabled' => true
-    ]
-];
